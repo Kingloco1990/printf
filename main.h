10c15393f466546aac016d9856dc7229f7368c47
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-21 06:14:45 -0700

    Commit for main.h file

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdarg.h>
-int _printf(const char *format, ...);
+#include<stdarg.h>
 int _putchar(char c);
+int _printf(const char *format, ...);
 #endif

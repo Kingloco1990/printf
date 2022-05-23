e111ff64c1f01382edc429a1750a7cac6d560af1
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-05-22 20:44:53 -0700

    Commit for main.h file

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
 #include<stdarg.h>
-int _putchar(char c);
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_char(va_list);
+int _print_str(va_list);
+int _print_int(va_list);
+int(*_select_func(char c))(va_list);
+
 #endif
+

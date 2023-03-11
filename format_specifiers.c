dc52bb11be98db72aa0e7e11b77552a129fc2204
Kingloco1990 <ransfordwelbeck@gmail.com>
2023-03-11 06:35:05 -0800

    Commit for the 0x11. C - printf project

--- format_specifiers.c
+++ format_specifiers.c
@@ -55,3 +55,30 @@ int print_percent(va_list arg __attribute__((unused)))
 
 	return (1);
 }
+
+/**
+ * print_Sstring - Prints characters not in Not in C99 or C11
+ *                 as '\x'.
+ * @arg: A list of arguments to be printerd.
+ *
+ * Return: The number of '\x' characters printed.
+ */
+int print_Sstring(va_list arg)
+{
+	int i = 0;
+	char *str, *s;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i] != '\0' && ((str[i] < 0 && str[i] < 32) || str[i] >= 127))
+	{
+		s = "\\x";
+		write(1, s, i);
+		i++;
+	}
+
+	return (i);
+}

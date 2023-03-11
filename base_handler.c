a559ecc1ad65c4025e656e4d2d09ea1a7179a7c7
Kingloco1990 <ransfordwelbeck@gmail.com>
2023-03-11 03:05:00 -0800

    Commit for the 0x11. C - printf project

--- base_handler.c
+++ base_handler.c
@@ -144,87 +144,55 @@ int print_hex(va_list arg)
 	return (len);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * print_heX - Converts and prints decimal to uppercase hexadecimal.
+ * @arg: List of all the arguments passed to the program.
+ *
+ * Return: The number of characters printed.
+ */
+int print_hex(va_list arg)
+{
+	int len, i, result;
+	char *str, *rev_str, ch;
+	unsigned int num;
+
+	num = va_arg(arg, unsigned int);
+
+	if (num == 0)
+	{
+		ch = '0';
+		return (write(1, &ch, 1));
+	}
+
+	len = base_len(num, 16);
+
+	str = malloc(sizeof(char) * len + 1);
+
+	if (str == NULL)
+		return (-1);
+
+	for (i = 0; num > 0; i++)
+	{
+		result = num % 16;
+
+		if (result > 9)
+		{
+			result = hex_check(result, 'X');
+			str[i] = result;
+		}
+		else
+			str[i] = result + '0';
+
+		num = num / 16;
+	}
+
+	str[i] = '\0';
+	rev_str = rev_string(str);
+	if (rev_str == NULL)
+		return (-1);
+
+	write_base(rev_str);
+	free(str);
+	free(rev_str);
+	return (len);
+}
